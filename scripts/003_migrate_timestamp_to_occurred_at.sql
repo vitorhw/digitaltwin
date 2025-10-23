c82084470823d7bf5ec93f90a8ecfb0535b48b8e
Vitor <welzel855@hotmail.com>
2025-10-23 10:14:56 -0700

    clean up

--- scripts/003_migrate_timestamp_to_occurred_at.sql
+++
@@ -1,9 +0,0 @@
--- Migration: Rename timestamp column to occurred_at to avoid reserved keyword
--- Run this if you already created tables with the timestamp column
-
-ALTER TABLE episodic_memories 
-RENAME COLUMN timestamp TO occurred_at;
-
--- Update the index name for clarity
-DROP INDEX IF EXISTS idx_episodic_timestamp;
-CREATE INDEX idx_episodic_occurred_at ON episodic_memories(user_id, occurred_at DESC);
